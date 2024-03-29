0b4e16f5a6e90fcc7c45c3cc82710c1177bf1a65
Iarlen <iarlensr@hotmail.com>
2024-03-28 23:59:51 -0300

    refactor: modifying style on link active.

--- src/components/InternalLink/index.tsx
+++ src/components/InternalLink/index.tsx
@@ -18,7 +18,7 @@ export default function InternalLink({
       <Icon
         data-testid="internal-link-icon"
         className={clsx('size-5 transition-all hover:text-white', {
-          'text-white': active,
+          'animate-pulse text-white': active,
           'text-white/60': !active,
         })}
       />
