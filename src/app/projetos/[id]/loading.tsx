8052e2533f1fc105152a99be4eb526e4bc225653
Iarlen <iarlensr@hotmail.com>
2024-03-30 23:01:58 -0300

    feat: create page detail project.

---
+++ src/app/projetos/[id]/loading.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function ProjectDetailLoading() {
+  return (
+    <div className="mx-auto flex w-full max-w-[600px] flex-col gap-5 pb-12">
+      <div className="mt-4 flex items-center gap-3">
+        <div className="h-[50px] w-[50px] animate-pulse rounded-full bg-zinc-400"></div>
+        <div className=" flex flex-col gap-1.5">
+          <div className="h-[30px] w-[160px] animate-pulse rounded bg-zinc-400"></div>
+          <div className="h-[20px] w-[220px] animate-pulse rounded bg-zinc-400"></div>
+        </div>
+      </div>
+      <div className="h-[172px] w-full max-w-[500px] animate-pulse rounded-md bg-zinc-400 sm:h-[250px] sm:max-w-[600px] md:h-[320px]"></div>
+      <div className="flex items-center gap-3">
+        <div className="h-[46px] w-[46px] animate-pulse rounded-xl bg-zinc-400"></div>
+        <div className="h-[46px] w-[46px] animate-pulse rounded-xl bg-zinc-400"></div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="h-[25px] w-[160px] animate-pulse rounded bg-zinc-400"></div>
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
+          <div className="flex items-center gap-2">
+            <div className="h-[5px] w-[5px] animate-pulse rounded-full bg-zinc-400"></div>
+            <div className="h-[10px] w-full animate-pulse rounded bg-zinc-400"></div>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="h-[5px] w-[5px] animate-pulse rounded-full bg-zinc-400"></div>
+            <div className="h-[10px] w-full animate-pulse rounded bg-zinc-400"></div>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="h-[5px] w-[5px] animate-pulse rounded-full bg-zinc-400"></div>
+            <div className="h-[10px] w-full animate-pulse rounded bg-zinc-400"></div>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="h-[5px] w-[5px] animate-pulse rounded-full bg-zinc-400"></div>
+            <div className="h-[10px] w-full animate-pulse rounded bg-zinc-400"></div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="h-[25px] w-[200px] animate-pulse rounded bg-zinc-400"></div>
+        <div className="flex flex-col gap-3">
+          <div className="h-[150px] w-full animate-pulse rounded bg-zinc-400"></div>
+          <div className="h-[100px] w-full animate-pulse rounded bg-zinc-400"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/projetos/[id]/page.tsx
@@ -0,0 +1,105 @@
+import { ChevronLeft, DotIcon, GithubIcon, RocketIcon } from 'lucide-react'
+import ExternalLink from '@/components/ExternalLink'
+import { api } from '@/utils/api'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+interface ParamProps {
+  params: {
+    id: string
+  }
+}
+
+interface ProjectProps {
+  id: string
+  name: string
+  type: string
+  image: string
+  deploy: string
+  github: string
+  started: string
+  finished: string
+  description: string
+  technologies: string[]
+}
+
+export async function generateMetadata({ params }: ParamProps) {
+  const response = await api.get<ProjectProps>(`/projects/${params.id}`)
+
+  const project = response.data
+
+  return {
+    title: `${project.name} - Iarlen Reis`,
+    description: project.description,
+    keywords: project.technologies,
+  }
+}
+
+export default async function ProjectDetailPage({ params }: ParamProps) {
+  const response = await api.get<ProjectProps>(`/projects/${params.id}`)
+
+  const project = response.data
+
+  return (
+    <div className="mx-auto flex w-full max-w-[600px] flex-col gap-5 pb-12">
+      <div className="mt-4 flex items-center gap-3 md:gap-3.5">
+        <Link
+          href="/projetos"
+          aria-label="Voltar para a página de projetos"
+          className="w-fit rounded-full border border-white/40 p-1"
+        >
+          <ChevronLeft className="size-6" />
+        </Link>
+        <div className=" flex flex-col gap-1.5">
+          <h1 className="text-2xl">{project.name}</h1>
+          <p className="text-sm text-zinc-300">
+            {project.started} - {project.finished}
+          </p>
+        </div>
+      </div>
+      <Image
+        priority
+        width={600}
+        height={250}
+        src={project.image}
+        alt={`Imagem do projeto ${project.name}`}
+        className="w-full max-w-[500px] rounded-md sm:max-w-[600px]"
+      />
+      <ul className="flex items-center gap-3">
+        <ExternalLink
+          icon={GithubIcon}
+          href={project.github}
+          aria-label="Link para o repositório no Github"
+          className="hover:bg-black/50"
+        />
+        <ExternalLink
+          icon={RocketIcon}
+          href={project.deploy}
+          aria-label="Link para o deploy da aplicação"
+          className="hover:bg-green-500/60"
+        />
+      </ul>
+      <div className="flex flex-col gap-2">
+        <h2 className="text-xl sm:text-2xl">Tecnologias</h2>
+        <ul className="grid grid-cols-2 gap-2 sm:grid-cols-3">
+          {project.technologies.map((technology) => (
+            <li
+              key={technology}
+              className="flex items-center gap-1 text-sm text-zinc-300 sm:text-base"
+            >
+              <DotIcon className="size-6" />
+              {technology}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h2 className="text-xl sm:text-2xl">Sobre o projeto</h2>
+        <pre className="whitespace-pre-wrap text-zinc-300">
+          {project.description}
+        </pre>
+      </div>
+    </div>
+  )
+}
