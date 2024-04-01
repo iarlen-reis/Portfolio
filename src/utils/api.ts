7eb8eed400780c68c16a70abb0afa675288a70c5
Iarlen <iarlensr@hotmail.com>
2024-03-31 22:35:54 -0300

    feat: add .env on gitignore and create axios instance.

---
+++ src/utils/api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: process.env.API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+})
