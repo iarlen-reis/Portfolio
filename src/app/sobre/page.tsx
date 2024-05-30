948b58adcf555bd35f0a6a93cccc797540f2d28a
Iarlen <iarlensr@hotmail.com>
2024-05-30 02:45:58 -0300

    refactor: removing framework and databases sections.

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,23 +1,7 @@
-import { FaJava, FaPython, FaReact, FaSass } from 'react-icons/fa6'
+import { FaJava, FaPython, FaPhp } from 'react-icons/fa6'
 import IconCard from '@/components/IconCard'
 import {
-  SiExpo,
-  SiFastify,
-  SiFirebase,
-  SiMongodb,
-  SiMysql,
-  SiPostgresql,
-  SiPrisma,
-  SiRadixui,
-  SiSpring,
-  SiSpringsecurity,
-  SiStyledcomponents,
-  SiTailwindcss,
-} from 'react-icons/si'
-import {
-  TbBrandCss3,
   TbBrandJavascript,
-  TbBrandNextjs,
   TbBrandNodejs,
   TbBrandTypescript,
 } from 'react-icons/tb'
@@ -56,7 +40,7 @@ export default function AboutPage() {
           aprender react native.
         </p>
       </div>
-      <div className="mt-4 grid max-w-[1024px] grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 md:gap-10 lg:grid-cols-4">
+      <div className="mt-4 grid max-w-[1024px] grid-cols-1">
         <div className="flex w-fit flex-col gap-4">
           <h2 className="text-xl md:text-2xl">Linguagens</h2>
           <ul className="flex flex-wrap items-center justify-start gap-3">
@@ -90,116 +74,11 @@ export default function AboutPage() {
               color="#F99916"
               aria-label="Linguagem Java"
             />
-          </ul>
-        </div>
-        <div className="flex w-fit flex-col gap-4">
-          <h2 className="text-xl md:text-2xl">Frameworks</h2>
-          <ul className="flex flex-wrap items-center justify-start gap-3">
             <IconCard
-              title="ReactJs"
-              icon={FaReact}
-              color="#61DBFB"
-              aria-label="Framework React"
-            />
-            <IconCard
-              title="NextJs"
-              icon={TbBrandNextjs}
-              color="#FFFFFF"
-              aria-label="Framework NextJs"
-            />
-            <IconCard
-              title="Expo"
-              icon={SiExpo}
-              color="#FFFFFF"
-              aria-label="Framework Expo"
-            />
-            <IconCard
-              title="Spring"
-              icon={SiSpring}
-              color="#6DB33F"
-              aria-label="Framework Spring"
-            />
-            <IconCard
-              title="Spring Security"
-              icon={SiSpringsecurity}
-              color="#6DB33F"
-              aria-label="Framework Spring"
-            />
-            <IconCard
-              title="Fastify"
-              icon={SiFastify}
-              color="#FFFFFF"
-              aria-label="Framework Fastify"
-            />
-          </ul>
-        </div>
-        <div className="flex w-fit flex-col gap-4">
-          <h2 className="text-xl md:text-2xl">Estilizações</h2>
-          <ul className="flex flex-wrap items-center justify-start gap-3">
-            <IconCard
-              title="Tailwind"
-              color="#61DBFB"
-              icon={SiTailwindcss}
-              aria-label="Tailwind"
-            />
-            <IconCard
-              title="Sass"
-              color="#cd6799"
-              icon={FaSass}
-              aria-label="Sass"
-            />
-            <IconCard
-              title="CSS"
-              color="#264de4"
-              icon={TbBrandCss3}
-              aria-label="CSS"
-            />
-            <IconCard
-              color="#db086e"
-              title="Styled components"
-              icon={SiStyledcomponents}
-              aria-label="Styled components"
-            />
-            <IconCard
-              color="#FFFFFF"
-              icon={SiRadixui}
-              title="Radix UI"
-              aria-label="Radix UI"
-            />
-          </ul>
-        </div>
-        <div className="flex w-fit flex-col gap-4">
-          <h2 className="text-xl md:text-2xl">Database</h2>
-          <ul className="flex flex-wrap items-center justify-start gap-3">
-            <IconCard
-              title="Mysql"
-              color="#F99916"
-              icon={SiMysql}
-              aria-label="Banco de dados Mysql"
-            />
-            <IconCard
-              title="Postgres"
-              color="#396c94"
-              icon={SiPostgresql}
-              aria-label="Banco de dados Postgres"
-            />
-            <IconCard
-              title="Mongo DB"
-              icon={SiMongodb}
-              color="#01ed65"
-              aria-label="Banco de dados Mongo DB"
-            />
-            <IconCard
-              title="Firebase"
-              icon={SiFirebase}
-              color="#ffa714"
-              aria-label="Banco de dados Firebase"
-            />
-            <IconCard
-              title="Prisma"
-              icon={SiPrisma}
-              color="#FFFFFF"
-              aria-label="Prisma ORM"
+              title="PHP"
+              icon={FaPhp}
+              color="#787CB4"
+              aria-label="Linguagem PHP"
             />
           </ul>
         </div>
